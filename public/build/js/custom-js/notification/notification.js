e46bf9a12710d085c7ed258648c5051a152199ee
unknown <salmanrazabwn@gmail.com>
2024-06-28 13:15:39 +0500

    notifications routes

--- public/build/js/custom-js/notification/notification.js
+++ public/build/js/custom-js/notification/notification.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('.notificationCounter').text(0);
 
     window.Echo.channel("notificationChannel").listen("NotificationEvent", (e) => {
-        console.log(e.notificationData.length);
+
 
         if(e.notificationData.length > 0){
             $('empty-notification-elem').addClass('d-none');
@@ -25,6 +25,7 @@ $(document).ready(function(){
                 $('.notification-text').text(row.content);
             }
 
+
             notificationHtml+='<div class="text-reset notification-item d-block dropdown-item position-relative btn-read-notification" data="'+row.id+'">'+
                 '<div class="d-flex">'+
                 '<div class="avatar-xs me-3">'+
@@ -33,7 +34,7 @@ $(document).ready(function(){
                 '</span>'+
                 '</div>'+
                 '<div class="flex-1">'+
-                '<a href="'+row.url+'" class="stretched-link">'+
+                '<a href="'+route('admin.orders.list')+'" class="stretched-link">'+
                 // '<h6 class="mt-0 mb-2 fs-13 lh-base">You have received <b class="text-success">20</b> new messages in the conversation   </h6>'+
                 '<h6 class="mt-0 mb-2 fs-13 lh-base">'+row.content+'</h6>'+
 
@@ -117,11 +118,15 @@ $(document).ready(function(){
         cluster: 'ap2'
     });
 
-    var channel = pusher.subscribe('examData');
-    channel.bind('App\\Events\\CourseEvent', function(data) {
-        console.log('My exam data',data.data.stdName);
-   
+    var channel = pusher.subscribe('notificationChannel');
+    channel.bind('App\\Events\\NotificationEvent', function(e) {
 
+        if(e.notificationData.length > 0){
+            $('empty-notification-elem').addClass('d-none');
+            fnShowNotifications(e.notificationData,1);
+        }else{
+            $('empty-notification-elem').removeClass('d-none');
+        }
 
     });
 });
