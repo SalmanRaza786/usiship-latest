8dd6e2577f8b2eab83284c21d5c2e09677939525
unknown <salmanrazabwn@gmail.com>
2024-07-29 15:58:35 +0500

    working on out bounds

---
+++ database/migrations/2024_07_29_104717_create_work_order_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('work_order_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('work_order_id');
+            $table->foreign('work_order_id')->references('id')->on('work_orders')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('inventory_id');
+            $table->foreign('inventory_id')->references('id')->on('inventories')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('loc_id');
+            $table->foreign('loc_id')->references('id')->on('wh_locations')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('qty');
+            $table->string('pallet_number');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('work_order_items');
+    }
+};
