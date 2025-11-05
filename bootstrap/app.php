8a6030ee01923f145596212e4422b7bedcaf79e4
MAIT <faheem.akram@mait.ae>
2025-11-05 10:54:08 +0500

    pdo change

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,7 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'isUser' => \Illuminate\Auth\Middleware\Authenticate::class,
         ]);
 //        $middleware->append(\App\Http\Middleware\NormalizeJsonNumbers::class);
-        $middleware->appendToGroup('api', \App\Http\Middleware\NormalizeJsonNumbers::class);
+        $middleware->appendToGroup('api', \App\Http\Middleware\NormalizeJsonIds::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
